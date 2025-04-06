620c4bd9e45d0489118bdb33b6bfedc3fba35e45
Yash Bharti <yashbharti004@gmail.com>
2025-04-06 14:01:40 +0530

    Update firebase-config.js

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -3,14 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAMmF_FohqS60yLIVs3KNWAFTnH7EWAhMI",
-    authDomain: "studysync-9ba73.firebaseapp.com",
-    databaseURL: "https://studysync-9ba73-default-rtdb.firebaseio.com",
-    projectId: "studysync-9ba73",
-    storageBucket: "studysync-9ba73.firebasestorage.app",
-    messagingSenderId: "390754530788",
-    appId: "1:390754530788:web:103c93e185db0f1cad03f1",
-    measurementId: "G-DEV9KS8CDG"
+    
   };
 
 // Initialize Firebase
